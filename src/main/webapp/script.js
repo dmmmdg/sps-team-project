6fc20c18e667f2bbdf2cdad07b266aa74112b0cb
Yuntian Wu <triple_h@outlook.com>
2022-07-07 20:57:01 +0000

    Sign In feature optimazied

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -13,16 +13,27 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds the sign in with Google function.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function handleCredentialResponse(response) {
+      const data = jwt_decode(response.credential)
+      console.log(data)
+      document.getElementById('fullName').textContent = data.name
+      document.getElementById('sub').textContent = data.sub
+      document.getElementById('given_name').textContent = data.given_name
+      document.getElementById('family_name').textContent = data.family_name
+      document.getElementById('email').textContent = data.email
+      document.getElementById('verifiedEmail').textContent = data.email_verified
+      document.getElementById('picture').setAttribute("src", data.picture)
+  }
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "891385536925-sblb9nv3ehfc45p806jkkarnvjivb9he.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
 }
